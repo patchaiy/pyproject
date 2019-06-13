951a35d9b9d1a1bfabe4418cdd8e7fe6807a40e9
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-06-13 17:27:24 +0530

    Update and rename armstrongno.py to armstrongnumber.py

--- armstrongnumber.py
+++ armstrongnumber.py
@@ -2,11 +2,11 @@ lower=int(input())
 upper = int(input())
 for num in range(lower, upper + 1):
    order = len(str(num))
-   sum = 0
+   s= 0
    t= num
    while t > 0:
        digit = t % 10
-       sum += digit ** order
+       s+= digit ** order
        t//= 10
-  if num == sum:
+  if num == s:
        print(num)
